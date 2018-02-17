e447714f3059d6c5943956a8926876d78c70b562
kench1994 <kench.h.wong@gmail.com>
2018-02-17 22:30:40 +0800

    加入直方图统计

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,17 +3,17 @@
 #include <QDebug>
 #include <QDir>
 #include <QVector>
-#include <stdio.h>
+#include <QTime>
+
 #define MAX(x,y) ((x>y)?x:y)
 #define MIN(x,y,z) (((x>y?y:x)>z)?z:(x>y?y:x))
 
-
-/*求解最大公共子序列,用于字符串相似度比较
- * 可以知道我现在获得切片数据和字模其实直接用比较运算就可以得出结果
- * 2018-02-16,发现使用该算法存在bug,比如4,+,-这几个符号与-相比较所得相似度一样
+QTime time;
+/* 求解最大公共子序列,用于字符串相似度比较
+ * 2018-02-16,发现如果只使用该算法将存在bug,比如4,+,-这几个符号与-相比较所得相似度一样
  * 蛤铪^_^
 */
-int LongestCommonSubsequence(QString strFirst, QString strSecond) {
+int LongestCommonSubsequence(const QString& strFirst, const  QString& strSecond) {
     int nLenOfFirstStr = strFirst.length();    int nLenOfSecondStr = strSecond.length();
     QVector<QVector<int>> matrix(nLenOfFirstStr+1, QVector<int>(nLenOfSecondStr+1));
     for (int i = 0; i <= nLenOfFirstStr; i++) {
@@ -30,41 +30,32 @@ int LongestCommonSubsequence(QString strFirst, QString strSecond) {
     return matrix[nLenOfFirstStr][nLenOfSecondStr];
 }
 
-int LevenshteinDistance(QString strFirst, QString strSecond){
+int LevenshteinDistance(const QString& strFirst, const QString& strSecond){
     int n = strFirst.length();
     int m = strSecond.length();
     if (n == 0)
        return n;
     if (m == 0)
        return m;
-
+    int above,left,diag,cost;
+    QChar si,dj;
     QVector<QVector<int>> matrix(n+1, QVector<int>(m+1));
-    for (int i = 1; i <= n; i++)
-    {
-        const QCharRef si = strFirst[i - 1];
-        for (int j = 1; j <= m; j++)
-        {
-
-            const QCharRef dj = strSecond[j - 1];
-            //step 5
-            int cost;
-            if (si == dj){
-                cost = 0;
-            }
-            else{
-                cost = 1;
-            }
-            const int above = matrix[i - 1][j] + 1;
-            const int left = matrix[i][j - 1] + 1;
-            const int diag = matrix[i - 1][j - 1] + cost;
+    for (int i = 1; i <= n; i++){
+        si = strFirst[i - 1];
+        for (int j = 1; j <= m; j++){
+            dj = strSecond[j - 1];
+            if (si == dj) cost = 0;
+            else cost = 1;
+            above  = matrix[i - 1][j] + 1;
+            left = matrix[i][j - 1] + 1;
+            diag = matrix[i - 1][j - 1] + cost;
             matrix[i][j] = MIN(above, left, diag);
-
         }
     }
     return matrix[n][m];
 }
 
-float SimilarTextRate(QString strFirst, QString strSecond)
+float SimilarTextRate(const QString &strFirst, const QString &strSecond)
 {
     int lcs = LongestCommonSubsequence(strFirst, strSecond);
     int ld = LevenshteinDistance(strFirst, strSecond);
@@ -139,6 +130,22 @@ inline int EdgeExtracting(const QImage* img,QList<QRect> &listWorldEdge){
 }
 
 
+int HistogramHorizon(QImage* binImg, QVector<int>& hist){
+    int nHigest = 0;
+    for (int i=0;i!=binImg->width();i++) {
+        for (int j=0;j!=binImg->height();j++){
+            if(!binImg->pixelColor(i,j).value()){
+                hist[i]++;
+            }
+        }
+        if(hist[i] > nHigest)
+            nHigest = hist[i];
+        //qDebug()<<hist[i];
+    }
+    return nHigest;
+}
+
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -150,19 +157,35 @@ MainWindow::MainWindow(QWidget *parent) :
     for(int i = 0; i <= 11 ;i++){
         //读取字模加载到listCharacterMatrix
         listCharacterMatrix<<GenCharacterMatrix("../bili2SliverOcr/CharacterMatrix/"+QString::number(i,10)+".png");
-        //qDebug()<<i<<":"<<GenCharacterMatrix("../bili2SliverOcr/CharacterMatrix/"+QString::number(i,10)+".png");
     }
 
     QImage oriImg("../bili2SliverOcr/getCaptcha.jpg");
-    ui->originalImg->show();
-    QImage binaryImage(oriImg.size(), QImage::Format_RGB32);
+    //ui->originalImg->show();
+    QImage binaryImage(oriImg.size(), QImage::Format_ARGB32);
+    QGraphicsScene* sceneOriImg;
+    time.start();
     BinProcessing(&oriImg,&binaryImage);
-
     //显示二值化后的图片
-    QGraphicsScene* sceneOriImg = new QGraphicsScene();
+    sceneOriImg = new QGraphicsScene();
     sceneOriImg->addPixmap(QPixmap::fromImage(binaryImage));
     ui->originalImg->setScene(sceneOriImg);
     ui->originalImg->show();
+
+
+    QVector<int> vectHist(binaryImage.width(),0);
+    int nHigest = HistogramHorizon(&binaryImage,vectHist);
+    QImage imgHist(binaryImage.width(),nHigest,QImage::Format_ARGB32);
+    for(int x=0;x<vectHist.length();x++){
+        for(int y=0;y<vectHist[x];y++)
+            imgHist.setPixel(x,y,qRgb(0, 0, 0));
+    }
+    sceneOriImg = new QGraphicsScene();
+    sceneOriImg->addPixmap(QPixmap::fromImage(imgHist));
+    ui->histogramVertical->setScene(sceneOriImg);
+    ui->histogramVertical->show();
+
+
+
 
     //提取图像边缘,并保存到list
     QList<QRect> listWorldEdge;
@@ -171,42 +194,28 @@ MainWindow::MainWindow(QWidget *parent) :
     QHash<int,int> hashSimilarity;
     int nSignPos = 0;
     for(int i=0;i<nSliceCount;i++){
-        qDebug()<<"cur:"<<i;
         int nMaxSimilarity = 0;
         //读取切片数据
         QString strCompare = "";
         for(int q = listWorldEdge.at(i).y();q<listWorldEdge.at(i).height()+listWorldEdge.at(i).y();q++)
-            for(int p=listWorldEdge.at(i).x();p<listWorldEdge.at(i).width()+listWorldEdge.at(i).x();p++){
+            for(int p=listWorldEdge.at(i).x();p<listWorldEdge.at(i).width()+listWorldEdge.at(i).x();p++)
                 strCompare+=(!binaryImage.pixelColor(p,q).value())?"0":"1";
-        }
-        //qDebug()<<strCompare;
+
+
         //相似度比较
         for(int j=0; j<listCharacterMatrix.length(); j++){
-//            //因为选取图片的标准性,所以可以直接使用比较运算得出结果,可以选择直接使用这种算法
-//            if(listCharacterMatrix.at(j) == strCompare)
-//            {
-//                hashSimilarity[i] = j;
-//                //qDebug()<< j;
-//                //qDebug()<<strCompare;
-//                if(j>9) nSignPos = i;
-//            }
-
             float nTempSimilarity = SimilarTextRate(listCharacterMatrix.at(j),strCompare);
-            qDebug()<< j << "->"<<nTempSimilarity;
+            //qDebug()<< j << "->"<<nTempSimilarity;
             if(nTempSimilarity>nMaxSimilarity) {
                 nMaxSimilarity=nTempSimilarity;
                 hashSimilarity[i] = j;
-                if(j>9) nSignPos = i;
+                if(j>9) nSignPos = i;//符号位
             }
         }
-        //qDebug()<<"\r\n";
     }
     //qDebug()<<nSignPos;
     int res1 = 0,res2 = 0,res = 0;
     for(int i = 0;i<nSignPos;i++){
-        //int nDigit = nSignPos-i-1;
-        //==0时说明当前为个位数
-        //res1 += (!nDigit)?hashSimilarity[i]:hashSimilarity[i]*qPow(10,nDigit);
         res1*=10;
         res1+=hashSimilarity[i];
     }
@@ -224,7 +233,9 @@ MainWindow::MainWindow(QWidget *parent) :
         res = res1-res2;
     else
         qDebug()<<"none find sign symbol";
+    qDebug()<<time.elapsed()/1000.0<<"s";
     ui->textEdit->setText(QString::number(res,10));
+
 }
 
 MainWindow::~MainWindow()
